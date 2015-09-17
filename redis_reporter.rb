922b5e03ff9e922c25e3043c8185cfe361a88667
Aubrey Holland <aubreyholland@gmail.com>
2015-09-16 21:37:48 -0400

    handle one weird key/value

--- redis_reporter.rb
+++ redis_reporter.rb
@@ -8,7 +8,15 @@ info = `/usr/local/bin/redis-cli INFO`
 
 data = {}
 info.split("\n").each do |line|
-  if line =~ /\w+:\w+/
+  if line =~ /(\w+):keys=(\d+),expires=(\d+),avg_ttl=(\d+)/
+    data['keys'] ||= {}
+    data['expires'] ||= {}
+    data['avg_ttl'] ||= {}
+    database = $1
+    data['keys'][database] = $2.to_i
+    data['expires'][database] = $3.to_i
+    data['avg_ttl'][database] = $4.to_i
+  elsif line =~ /\w+:\w+/
     parts = line.split(':')
     data[parts[0].strip] = parts[1].strip
   end
@@ -19,8 +27,18 @@ params = YAML.load(File.open(ARGV[0], 'r').read)
 Librato::Metrics.authenticate params['librato']['user'], params['librato']['token']
 
 params['metrics'].each do |metric|
-  met = {}
-  met["redis.#{metric}"] = { value: data[metric], source: params['source'] }
-  puts "Reporting #{metric} as #{data[metric]}"
-  Librato::Metrics.submit met
+  if data[metric].is_a?(Hash)
+    data[metric].each do |db, value|
+      source = "#{params['source']}.#{db}"
+      met = {}
+      met["redis.#{metric}"] = { value: value, source: source }
+      puts "Reporting #{metric} as #{data[metric]} for source #{source}"
+      Librato::Metrics.submit met
+    end
+  else
+    met = {}
+    met["redis.#{metric}"] = { value: data[metric], source: params['source'] }
+    puts "Reporting #{metric} as #{data[metric]}"
+    Librato::Metrics.submit met
+  end
 end
